869447d7b314fe146bd547949db6342efaa47f34
Mateo Presa Castro <mateopresacastro@gmail.com>
2023-01-30 09:51:16 +0100

    Remove redux

--- geopoints/pages/_app.tsx
+++ geopoints/pages/_app.tsx
@@ -1,14 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import store from '../redux/store';
-import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />;
-    </Provider>
-  );
+  return <Component {...pageProps} />
 }
 
 export default MyApp;
