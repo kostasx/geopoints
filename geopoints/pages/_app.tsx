c3935bbf1ac6ff57353d82d52dcf085006ead9f6
Dom Bolton <dominicbolton11@gmail.com>
2023-01-30 10:37:45 +0000

    feat: login functionality

--- geopoints/pages/_app.tsx
+++ geopoints/pages/_app.tsx
@@ -2,12 +2,15 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import store from '../redux/store';
 import { Provider } from 'react-redux';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />;
-    </Provider>
+    <UserProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />;
+      </Provider>
+    </UserProvider>
   );
 }
 
